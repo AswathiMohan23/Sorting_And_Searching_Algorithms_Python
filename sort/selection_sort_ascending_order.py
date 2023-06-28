aedf92ec360c6640d50c46f31f88f8acb2ab99d9
AswathiMohan23 <achu999here@gmail.com>
2023-06-28 16:20:56 +0530

    [ASWATHI] ADDED . example code for selection sort in ascending order

--- sort/selection_sort_ascending_order.py
+++ sort/selection_sort_ascending_order.py
@@ -1,7 +1,9 @@
+# selection sort in ascending order
 list1=[56,3,2,78,6,0]
 for i in range(len(list1)):
     min_val=min(list1[i:]) #find out min value and place at oth index
-    min_index=list1.index(min_val) # finding index of min value
+    min_index=list1.index(min_val,i) # finding index of min value... here i is given for the code to work
+                    # for duplicate values
     list1[i],list1[min_index]=list1[min_index],list1[i] # swapping the index
 
 print(list1)
