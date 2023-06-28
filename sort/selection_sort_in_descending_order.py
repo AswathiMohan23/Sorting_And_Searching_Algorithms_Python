7dd316bfc437ed8f52df77678c5efdc8f4fb5fee
AswathiMohan23 <achu999here@gmail.com>
2023-06-28 16:22:01 +0530

    [ASWATHI] ADDED . example code for selection sort in descending order

--- sort/selection_sort_in_descending_order.py
+++ sort/selection_sort_in_descending_order.py
@@ -3,8 +3,9 @@
 if __name__=="__main__":
     list1=[56,3,2,78,6,0]
     for i in range(len(list1)):
-        max_val=max(list1[i:]) #find out min value and place at oth index
-        max_index=list1.index(max_val) # finding index of min value
+        max_val=max(list1[i:]) #find out max value and place at zero th index
+        max_index=list1.index(max_val)# # finding index of max value... here i is given for the code to work
+                    # for duplicate values
         list1[i],list1[max_index]=list1[max_index],list1[i] # swapping the index
 
     print(list1)
